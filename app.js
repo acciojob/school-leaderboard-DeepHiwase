3616ba390f9b232c7cb556132b7a3307adf5a943
Deep <diphiwase0987@gmail.com>
2026-03-23 11:18:01 +0530

    solved

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const config = require("./config.json");
 
 //== connect to database
 const mongoURI =
-  config.MONGODB_URI || "mongodb://localhost:27017" + "/newsFeed";
+  /* config.MONGODB_URI ||  */"mongodb://localhost:27017" + "/newsFeed";
 
 let mongoose = require("mongoose");
 const Leaderboard = require("./model");
@@ -29,6 +29,20 @@ app.get("/", (req, res) => {
 });
 
 // your code here!
+app.get("/topRankings", async (req, res) => {
+  try {
+    let { limit = 20, offset = 0 } = req.query;
+
+    limit = parseInt(limit);
+    offset = parseInt(offset);
+
+    const data = await Leaderboard.find().sort({ global_rank: 1 }).skip(offset).limit(limit);
+
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // ==end==
 
